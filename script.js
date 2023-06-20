b312146a8e937bfc5f8cdc2b085c64a0f270643b
jenstem <124745888+jenstem@users.noreply.github.com>
2023-06-20 16:16:40 -0400

    Add js pseudo

---
+++ script.js
@@ -0,0 +1,23 @@
+// variable for questions
+
+// questions 1 - 5
+
+// answers 1 - 5
+
+// when to show correct and incorrect
+
+// timer function
+
+// keeping score
+
+// show score
+
+// highscores
+
+// submit
+
+// put highscores in order
+
+// go back
+
+// clear highscores
