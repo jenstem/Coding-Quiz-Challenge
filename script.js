fc4d2107d9fac2ddb794b289986a6629c28150d7
jenstem <124745888+jenstem@users.noreply.github.com>
2023-06-21 11:38:06 -0400

    Add empty var js

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 // variables
 
 // click start button var
-
+var startButton = document.querySelector("");
 // timer var
-
+var gameTimer = document.querySelector("");
 // question var
-
+var askQuestion = document.querySelector("");
 // answer var
-
+var userAnswer = document.querySelector("");
+// answer buttons
+var answerButton = document.querySelector("");
 // incorrect answer var
-
+var incorrectAnswer = document.querySelector("");
 // correct answer var
-
+var correctAnswer = document.querySelector("");
 // score var
-
+var score = 0;
 // how much time is left var = same as timer var?
-
+var timer;
+var timerCount;
 // end of game var
-
+var endGame = document.querySelector("");
 // initials var
-
+var getInitials = document.querySelector("");
 // highscores var
-
+var highScores = document.querySelector("");
+var highScoresList = "";
+// go back button
+var goBackButton = document.querySelector("");
+// clear scores button
+var clearScoresButton = document.querySelector("");
 // variable for questions
 
 // questions 1 - 5
