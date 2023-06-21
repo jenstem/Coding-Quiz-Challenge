f6a0bf3dd81d80e9af7f250cc687cea5babacb1c
jenstem <124745888+jenstem@users.noreply.github.com>
2023-06-21 11:54:11 -0400

    Add classes js

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 // variables
 
 // click start button var
-var startButton = document.querySelector("");
+var startButton = document.querySelector(".start-button");
 // timer var
-var gameTimer = document.querySelector("");
+var gameTimer = document.querySelector(".game-timer");
 // question var
-var askQuestion = document.querySelector("");
+var askQuestion = document.querySelector(".ask-question");
 // answer var
-var userAnswer = document.querySelector("");
+var userAnswer = document.querySelector(".user-answer");
 // answer buttons
-var answerButton = document.querySelector("");
+var answerButton = document.querySelector(".answer-button");
 // incorrect answer var
-var incorrectAnswer = document.querySelector("");
+var incorrectAnswer = document.querySelector(".incorrect");
 // correct answer var
-var correctAnswer = document.querySelector("");
+var correctAnswer = document.querySelector(".correct");
 // score var
 var score = 0;
 // how much time is left var = same as timer var?
 var timer;
 var timerCount;
 // end of game var
-var endGame = document.querySelector("");
+// var endGame = document.querySelector(".end-game");
+var endOfGame;
 // initials var
-var getInitials = document.querySelector("");
+var getInitials = document.querySelector(".get-initials");
 // highscores var
-var highScores = document.querySelector("");
+var highScores = document.querySelector(".highscores");
 var highScoresList = "";
 // go back button
-var goBackButton = document.querySelector("");
+var goBackButton = document.querySelector(".goback-button");
 // clear scores button
-var clearScoresButton = document.querySelector("");
+var clearScoresButton = document.querySelector("clear-scores");
 // variable for questions
 
 // questions 1 - 5
