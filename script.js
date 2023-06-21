0ea46018c36353387deb39f20b3e5db633ba43f7
jenstem <124745888+jenstem@users.noreply.github.com>
2023-06-21 11:20:51 -0400

    Update js pseudo

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 // variables
 
+// click start button var
+
+// timer var
+
+// question var
+
+// answer var
+
+// incorrect answer var
+
+// correct answer var
+
+// score var
+
+// how much time is left var = same as timer var?
+
+// end of game var
+
+// initials var
+
+// highscores var
+
 // variable for questions
 
 // questions 1 - 5
